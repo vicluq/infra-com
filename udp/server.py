ed6027dc01a10209d9b1e8b5d0119a1c8ad6723d
vicluq <victoria.tewari@gmail.com>
2023-12-05 09:03:13 -0300

    feat: finished server class

--- udp/server.py
+++ udp/server.py
@@ -5,7 +5,7 @@ import os
 
 # SocketKind ref: https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.sockettype?view=net-8.0
 
-# CLASS BOILERPLATE
+# ! CLASS BOILERPLATE
 class UDPServer:
     def __init__(self, sckt_family, sckt_type, sckt_binding, MAX_BUFF) -> None:
         self.sckt = skt.socket(sckt_family, sckt_type)
@@ -26,8 +26,13 @@ class UDPServer:
         return total_packs
     
 
+    def check_file(self, f_name):
+        return os.path.exists(f_name)
+
+
     def run(self, target_address):
         try:
+            content = ''
             while not self.stop:
                 if not self.init:
                     data, origin = self.sckt.recvfrom(self.MAX_BUFF)
@@ -35,12 +40,16 @@ class UDPServer:
 
                     state, content = data.split('_') # content = img or text
 
-                    self.init = 1 if state == "READY" else 0 # Start transmission when client is ready
+                    transmit = content and self.check_file(f'./samples/{content}')
+                    if not transmit:
+                        self.sckt.sendto(f"404:{content}", target_address)
+                        time.sleep(0.0001)
+
+                    self.init = 1 if state == "READY" and transmit else 0 # Start transmission when client is ready
                     self.stop = 1 if state == "STOP" else 0  # Stop socket
 
-                    cont_req = content if content else 'TEXT'
                 else:
-                    f_name = cont[cont_req]
+                    f_name = f'./samples/{content}'
                     file_buff = open(f_name, 'rb') # Reading binary file
                     total_pckts = self.get_packet_amout(f_name, self.MAX_BUFF)
                     
@@ -56,65 +65,82 @@ class UDPServer:
 
                     print('Done')
                     self.init = 0
+
+            self.close()
         except Exception as err:
             print(err)
 
 
-# PROCEDURAL
-def get_packet_amout(self, file):
-    fsize = os.stat(file).st_size # Size in bytes
-    total_packs = math.ceil(fsize/self.MAX_BUFF)
-    return total_packs
+    def close(self):
+        self.sckt.close()
 
 
-datagram_sckt = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
-datagram_sckt.bind(('127.0.0.1', 8080)) # Binding this address to the server
-datagram_sckt.settimeout(0.1)
+# ! PROCEDURAL
+def main():
+    def get_packet_amout(self, file):
+        fsize = os.stat(file).st_size # Size in bytes
+        total_packs = math.ceil(fsize/self.MAX_BUFF)
+        return total_packs
 
-if datagram_sckt is None:
-    raise "Socket not available."
+
+    datagram_sckt = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
+    datagram_sckt.bind(('127.0.0.1', 8080)) # Binding this address to the server
+    datagram_sckt.settimeout(0.1)
+
+    if datagram_sckt is None:
+        raise "Socket not available."
 
 
-target_address = ('127.0.0.1', 7070) # Host, Port
-MAX_BUFFER_SIZE = 1024 # Bytes
+    target_address = ('127.0.0.1', 7070) # Host, Port
+    MAX_BUFFER_SIZE = 1024 # Bytes
 
-init = 0
-stop = 0
-cont_req = 'TEXT'
-cont = {
-    'IMG': '',
-    'TEXT': './samples/all_too_well.txt'
-}
+    init = 0
+    stop = 0
+    cont_req = 'TEXT'
+    cont = {
+        'IMG': '',
+        'TEXT': './samples/all_too_well.txt'
+    }
 
-# Communication
-try:
-    while not stop:
-        if not init:
-            data, origin = datagram_sckt.recvfrom(MAX_BUFFER_SIZE)
-            data = data.decode() # From bytes to str
+    # Communication
+    try:
+        while not stop:
+            if not init:
+                data, origin = datagram_sckt.recvfrom(MAX_BUFFER_SIZE)
+                data = data.decode() # From bytes to str
 
-            state, content = data.split('_') # content = img or text
+                state, content = data.split('_') # content = img or text
 
-            init = 1 if state == "READY" else 0 # Start transmission when client is ready
-            stop = 1 if state == "STOP" else 0  # Stop socket
+                init = 1 if state == "READY" else 0 # Start transmission when client is ready
+                stop = 1 if state == "STOP" else 0  # Stop socket
 
-            cont_req = content if content else 'TEXT'
-        else:
-            f_name = cont[cont_req]
-            file_buff = open(f_name, 'rb') # Reading binary file
-            total_pckts = get_packet_amout(f_name, MAX_BUFFER_SIZE)
-            
-            datagram_sckt.sendto(f"INFO:{total_pckts}", target_address)
-            time.sleep(0.0001)
-
-            pckt = 1
-            while file_buff:
-                print(f"packet {pckt}/{total_pckts}")
-                datagram_sckt.sendto(file_buff.read(MAX_BUFFER_SIZE), target_address)
-                pckt += 1
+                cont_req = content if content else 'TEXT'
+            else:
+                f_name = cont[cont_req]
+                file_buff = open(f_name, 'rb') # Reading binary file
+                total_pckts = get_packet_amout(f_name, MAX_BUFFER_SIZE)
+                
+                datagram_sckt.sendto(f"INFO:{total_pckts}", target_address)
                 time.sleep(0.0001)
 
-            print('Done')
-            init = 0
-except Exception as err:
-    print(err)
+                pckt = 1
+                while file_buff:
+                    print(f"packet {pckt}/{total_pckts}")
+                    datagram_sckt.sendto(file_buff.read(MAX_BUFFER_SIZE), target_address)
+                    pckt += 1
+                    time.sleep(0.0001)
+
+                print('Done')
+                init = 0
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    # main()
+    MAX_BUFF_SIZE = 1024 # Bytes
+
+    addr_bind = ('127.0.0.1', 8080)
+    addr_target = ('127.0.0.1', 7070)
+
+    server = UDPServer(skt.AF_INET, skt.SOCK_DGRAM, addr_bind, MAX_BUFF_SIZE)
+    server.run(addr_target)
